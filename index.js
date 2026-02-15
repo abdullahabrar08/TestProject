6ff8468605f2894300715f4aaf9e22c441ca5f5c
ABDULLAH-PC\abdul <abdullahabrar08@gmail.com>
2026-02-15 16:56:30 +0300

    logs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   console.log("Received a request at /");
-  res.send("Server alive & kicking  with Docker 🚀 ....");
+  res.send("Server alive & kicking with Docker Containers 🚀 ....");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
